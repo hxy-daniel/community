94a4290d57d875b9df5dd769209227315e8a16a4
hxy-daniel <1054089446@qq.com>
2022-01-13 21:42:19 +0800

    完成登录功能

---
+++ src/main/java/com/cqupt/community/dao/LoginTicketDao.java
@@ -0,0 +1,25 @@
+package com.cqupt.community.dao;
+
+import com.cqupt.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface LoginTicketDao {
+    @Insert({
+            "insert into login_ticket (user_id, ticket, status, expired) ",
+            "values(#{userId}, #{ticket}, #{status}, #{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insertTicket(LoginTicket ticket);
+
+    @Select({
+            "select id, user_id, ticket, status, expired from login_ticket",
+            "where ticket = #{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    @Update({
+            "update login_ticket set status = #{status} where ticket = #{ticket}"
+    })
+    int updateStatus(String ticket, int status);
+}
